436202273eadba9937af82aec9455ca98d674170
Jakub <jakub.mandula@pbl.ee.ethz.ch>
2023-11-20 11:26:43 +0100

    add shm lim

--- metavision_docker.sh
+++ metavision_docker.sh
@@ -3,7 +3,7 @@
 xhost '+local:*'
 
 if [[ $(lshw -C display | grep vendor) =~ Nvidia ]]; then
-  docker run -it --rm --privileged --gpus all -v /dev/bus/usb:/dev/bus/usb -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix/ -v $(pwd):/home/`whoami`/pwd -e GDK_SCALE=0.5  registry.git.ee.ethz.ch/pbl/research/armasuisse/armasuisse-2023/metavisionsdk_docker/metavision22:latest  /bin/bash -c "metavision_studio; while /usr/bin/pgrep metavision >/dev/null; do sleep 1; done"
+  docker run -it --rm --privileged --gpus all -v /dev/bus/usb:/dev/bus/usb -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix/ -v $(pwd):/home/`whoami`/pwd -e GDK_SCALE=0.5  --shm-size 1000000000  registry.git.ee.ethz.ch/pbl/research/armasuisse/armasuisse-2023/metavisionsdk_docker/metavision22:latest  /bin/bash -c "metavision_studio; while /usr/bin/pgrep metavision >/dev/null; do sleep 1; done"
 else
-  docker run -it --rm --privileged  -v /dev/bus/usb:/dev/bus/usb -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix/ -v $(pwd):/home/`whoami`/pwd -e GDK_SCALE=0.5  registry.git.ee.ethz.ch/pbl/research/armasuisse/armasuisse-2023/metavisionsdk_docker/metavision22:latest  /bin/bash -c "metavision_studio; while /usr/bin/pgrep metavision >/dev/null; do sleep 1; done"
+  docker run -it --rm --privileged  -v /dev/bus/usb:/dev/bus/usb -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix/ -v $(pwd):/home/`whoami`/pwd -e GDK_SCALE=0.5  --shm-size 1000000000 registry.git.ee.ethz.ch/pbl/research/armasuisse/armasuisse-2023/metavisionsdk_docker/metavision22:latest  /bin/bash -c "metavision_studio; while /usr/bin/pgrep metavision >/dev/null; do sleep 1; done"
 fi
