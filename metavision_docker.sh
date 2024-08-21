6b10f7875eda5024c1acaed0c77501f4339eb017
Jakub Mandula <jakub.mandula@pbl.ee.ethz.ch>
2024-08-21 11:43:04 +0200

    fix container name in metavision_docker.sh

--- metavision_docker.sh
+++ metavision_docker.sh
@@ -8,4 +8,4 @@ else
   GPU_FLAG=
 fi
 
-docker run -it --rm --privileged $GPU_FLAG -v /dev/bus/usb:/dev/bus/usb -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix/ -v $(pwd):/home/`whoami`/pwd -v /home/$USER/.config/Metavision\ Studio:/home/$USER/.config/Metavision\ Studio -e GDK_SCALE=0.5  --shm-size 1000000000 metavision22_${USER}:latest  /bin/bash -c "metavision_studio; while /usr/bin/pgrep metavision >/dev/null; do sleep 1; done"
+docker run -it --rm --privileged $GPU_FLAG -v /dev/bus/usb:/dev/bus/usb -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix/ -v $(pwd):/home/`whoami`/pwd -v /home/$USER/.config/Metavision\ Studio:/home/$USER/.config/Metavision\ Studio -e GDK_SCALE=0.5  --shm-size 1000000000 metavisionsdk22_${USER}:latest  /bin/bash -c "metavision_studio; while /usr/bin/pgrep metavision >/dev/null; do sleep 1; done"
