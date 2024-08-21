26a2082a4e618c4318615a8793a17ef0cf7fb977
Jakub Mandula <jakub.mandula@pbl.ee.ethz.ch>
2024-08-21 12:30:00 +0200

    run the chmod script before docker starts and create .config folder if not exit

--- metavision_docker.sh
+++ metavision_docker.sh
@@ -8,4 +8,7 @@ else
   GPU_FLAG=
 fi
 
-docker run -it --rm --privileged $GPU_FLAG -v /dev/bus/usb:/dev/bus/usb -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix/ -v $(pwd):/home/`whoami`/pwd -v /home/$USER/.config/Metavision\ Studio:/home/$USER/.config/Metavision\ Studio -e GDK_SCALE=0.5  --shm-size 1000000000 metavisionsdk22_${USER}:latest  /bin/bash -c "metavision_studio; while /usr/bin/pgrep metavision >/dev/null; do sleep 1; done"
+#create config dir if it does not exist
+mkdir -p /home/$USER/.config/Metavision\ Studio
+
+docker run -it --rm --privileged $GPU_FLAG -v /dev/bus/usb:/dev/bus/usb -e DISPLAY=$DISPLAY -v /tmp/.X11-unix/:/tmp/.X11-unix/ -v $(pwd):/home/`whoami`/pwd -v /home/$USER/.config/Metavision\ Studio:/home/$USER/.config/Metavision\ Studio -e GDK_SCALE=0.5  --shm-size 1000000000 metavisionsdk22_${USER}:latest  /bin/bash -c "/scripts/chmod.sh && metavision_studio; while /usr/bin/pgrep metavision >/dev/null; do sleep 1; done"
