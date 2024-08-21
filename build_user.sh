e63958598490d5f77a0d25fc79d9a68b6b1302e8
Jakub Mandula <jakub.mandula@pbl.ee.ethz.ch>
2024-08-21 12:54:29 +0200

    set GROUP_ID and USER_ID in build script

--- build_user.sh
+++ build_user.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 if [ -z $USER_ID ]; then
-	echo "USER_ID varibale not set"
-	exit 1;
+	export USER_ID=`id -u`
+	echo "USER_ID varibale not set. Setting to `id -u`: $USER_ID"
 fi
 
 if [ -z $GROUP_ID ]; then
-	echo "GROUP_ID varible not set"
-	exit 1;
+	export GROUP_ID=`id -g`
+	echo "GROUP_ID varible not set. Setting to `id -g`: $GROUP_ID"
 fi
 
 docker compose build metavisionsdk
