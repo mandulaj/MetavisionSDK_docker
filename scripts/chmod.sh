2902b7011b651aff59e8fcbc7841b6d460882723
Jakub Mandula <jakub.mandula@pbl.ee.ethz.ch>
2024-08-21 12:30:34 +0200

    make the chmod skip sudo if not needed

--- scripts/chmod.sh
+++ scripts/chmod.sh
@@ -2,8 +2,18 @@
 
 CAMERAS=$(lsusb -d 04b4: |  awk '{print "/dev/bus/usb/"$2"/"$4}' |   sed 's/://' | tr '\n' ' ')
 
+
 if [ -n "$CAMERAS" ]; then
-	sudo chmod 666 $CAMERAS
+    for cam in $CAMERAS; do
+        current_perms=$(stat -c "%a" "$cam")       
+	
+        if [ "$current_perms" != "666" ]; then
+            echo "Changing permission of $cam to 666"                                                                                       
+	    sudo chmod 666 $cam
+        else
+            echo "$cam has correct permissions"
+	fi   
+    done
 else
-	echo "No Cameras detected"
+    echo "No Cameras detected"
 fi
